d5d8469a5876d8adfc963f80af03c861a07962f1
vakser <65089446+vakser@users.noreply.github.com>
2023-09-30 17:20:48 +0300

    Student controller servlet (controller) and list students jsp (view) created.

---
+++ src/main/java/com/example/web/jdbc/StudentControllerServlet.java
@@ -0,0 +1,47 @@
+package com.example.web.jdbc;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "StudentControllerServlet", value = "/student-controller-servlet")
+public class StudentControllerServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private StudentDbUtil studentDbUtil;
+
+    @Resource(name = "jdbc/web_student_tracker")
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            studentDbUtil = new StudentDbUtil(dataSource);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            listStudents(request, response);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<Student> students = studentDbUtil.getStudents();
+        request.setAttribute("student_list", students);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+        dispatcher.forward(request, response);
+    }
+
+}
